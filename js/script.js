43217f4b68626d1ef38ecffdeb61067bc2979606
James Barnett <barnettech@gmail.com>
2017-09-12 10:22:45 -0400

    box moving around.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshBasicMaterial( { color: 0x9932CC } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -15,10 +15,34 @@ camera.position.z = 5;
 var animate = function () {
 requestAnimationFrame( animate );
 
-cube.rotation.x += 0.1;
-cube.rotation.y += 0.1;
+cube.rotation.x += 0.05;
+cube.rotation.y += 0.05;
 
 renderer.render(scene, camera);
 };
 
 animate();
+
+
+// movement - please calibrate these values
+var xSpeed = 1;
+var ySpeed = 1;
+var xMovement = .75;
+var yMovement = .75;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        cube.position.y += yMovement;
+    } else if (keyCode == 83) {
+        cube.position.y -= yMovement;
+    } else if (keyCode == 65) {
+        cube.position.x -= xMovement;
+    } else if (keyCode == 68) {
+        cube.position.x += xMovement;
+    } else if (keyCode == 32) {
+        cube.position.set(0, 0, 0);
+    }
+};
+
